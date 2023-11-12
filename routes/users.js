9c60a967b9c13afb5efa0551cfdaecbee67bb9fd
ofir perez <fifop55@gmail.com>
2023-11-12 14:23:04 +0200

    second

--- routes/users.js
+++ routes/users.js
@@ -8,7 +8,7 @@ const router = express.Router();
 // מאזין לכניסה לראוט של העמוד בית לפי מה שנקבע לראוטר
 // בקובץ הקונפיג
 router.get("/", async(req,res) => {
-  res.json({msg:"Users endpoint"});
+  res.json({msg:"Users endpoint!"});
 })
 
 
